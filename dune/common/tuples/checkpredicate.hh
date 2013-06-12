d7f12749fa67b02e3402be46ad70db1dc5a2bf5c
Christoph Gersbacher <christoph.gersbacher@mathematik.uni-freiburg.de>
2013-06-12 11:16:50 +0200

    Added file 'checkpredicate.hh'.

---
+++ dune/common/tuples/checkpredicate.hh
@@ -0,0 +1,46 @@
+#ifndef DUNE_COMMON_TUPLES_CHECKPREDICATE_HH
+#define DUNE_COMMON_TUPLES_CHECKPREDICATE_HH
+
+#include <dune/common/tuples/tuples.hh>
+
+namespace Dune
+{
+
+  // CheckPredicate
+  // --------------
+
+  /** \class CheckPredicate
+   *
+   *  \brief Check a static predicate for all tuple elements.
+   *
+   *  \tparam  Tuple      Some tuple type
+   *  \tparam  Predicate  Unary static predicate
+   *  \tparam  N          Implementation internal
+   *
+   *  The predicate is expected to provide a static boolean member
+   *  \c v, i.e.,
+\code
+  template< class T >
+  struct Predicate
+  {
+    static const bool v = ...;
+  };
+\endcode
+   */
+  template< class Tuple, template< class > class Predicate,
+            int N = Dune::tuple_size< Tuple >::value >
+  struct CheckPredicate
+  {
+    static const bool value = ( Predicate< typename Dune::tuple_element< N-1, Tuple >::type >::value
+                                && CheckPredicate< Tuple, Predicate, (N-1) >::value );
+  };
+
+  template< class Tuple, template< class > class Predicate >
+  struct CheckPredicate< Tuple, Predicate, 0 >
+  {
+    static const bool value = true;
+  };
+
+} // namespace Dune
+
+#endif // #ifndef DUNE_COMMON_TUPLES_CHECKPREDICATE_HH
