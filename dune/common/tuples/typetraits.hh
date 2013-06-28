606212193f176d39d37fbb4190f9c4b7726ca37d
Christoph Gersbacher <christoph.gersbacher@mathematik.uni-freiburg.de>
2013-06-28 10:51:05 +0200

    Add struct 'RawTuple'.

--- dune/common/tuples/typetraits.hh
+++ dune/common/tuples/typetraits.hh
@@ -49,6 +49,90 @@ namespace Dune
     typedef typename ForEachType< IsReferencePredicate, Tuple >::Type ReferredTupleType;
   };
 
+
+
+  // RawTuple
+  // --------
+
+  /** \ingroup Tuples_Traits
+   *
+   *  \brief Access internal tuple type in wrappers.
+   *
+   *  \tparam  T  A Dune::tuple or a wrapper around a tuple.
+   *
+   *  Wrapper classes around a Dune::tuple may export the internal
+   *  tuple type in non-consistent way. Template specalizations
+   *  of Dune::RawTuple for wrappres solve this problem.
+   *
+   *  Dune::RawTuple exports the type of the wrapped tuple
+   *  and allows accessing it as well:
+   * \code
+   * template< class T >
+   * struct RawTuple
+   * {
+   *   typedef ImplementationDefined Type;
+   *
+   *   static Type &raw ( T &tuple );
+   * };
+   * \endcode
+   *
+   *  In order to behave like a Dune:tuple, Wrapper classes
+   *  need to overload Dune::tuple_element, Dune::tuple_size,
+   *  Dune::get, etc., which may become cumbersome.
+   *  Instead, it should always be safe to call
+   * \code
+   *   tempalte< int i, class T >
+   *   typename tuple_element< i, typename RawTuple< T >::Type >::type
+   *   get ( T &tuple )
+   *   {
+   *     return get< i >( RawTuple< T >::raw( t ) );
+   *   }
+   * \endcode
+   *
+   * See Dune::raw_tuple for a convenient alternative call to <tt>RawTuple< T >::raw( t )</tt>.
+   */
+  template< class T >
+  struct RawTuple;
+
+  template< class T >
+  struct RawTuple< const T >
+  {
+    typedef const typename RawTuple< T >::Type Type;
+
+    static Type &raw ( const T &tuple )
+    {
+      return RawTuple< T >::raw( const_cast< T & >( tuple ) );
+    }
+  };
+
+  template< class... T >
+  struct RawTuple< Dune::tuple< T... > >
+  {
+    typedef Dune::tuple< T... > Type;
+
+    static Type &raw ( Type &tuple ) { return tuple; }
+  };
+
+
+
+  // raw_tuple
+  // ---------
+
+  /** \ingroup Tuples_Traits
+   *
+   *  \brief Access internal tuple in wrappers.
+   *
+   *  \tparam  Tuple  A Dune::tuple or a wrapper around a tuple.
+   *
+   *  This convenience function is equivalent to
+   *  <tt>Dune::RawTuple< Tuple >::raw()</tt>.
+   */
+  template< class Tuple >
+  typename RawTuple< Tuple >::Type &raw_tuple ( Tuple &tuple )
+  {
+    return RawTuple< Tuple >::raw( tuple );
+  }
+
 } // namespace Dune
 
 #endif // #ifndef DUNE_COMMON_TUPLES_TYPETRAITS_HH
